55b0dee7fbaea46ab68c6c710fd687278fb0cfb2
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-19 19:41:48 +0700

    add pekerja

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -12,6 +12,15 @@ public:
     }
 };
 
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur) 
+    {
+        cout << "pekerja dibuat" << endl;
+    }
+};
+
 int main() {
     return 0;
 }
