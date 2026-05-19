2d0dfb65ce6bbeda77a72afa1d337e9c050ca3f6
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-19 19:29:01 +0700

    add class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,6 +11,15 @@ public:
     }
 };
 
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
 
 int main() {
     return 0;
